c92a47eddcd434a6e547d6b425b6947cdadeb653
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-05-22 15:56:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
     //Write your code here
-
+ let strng = document.getElementsByTagName("strong");
+	for(let i = 0;i< strng.length;i++) {
+		strng[i].style.color = "green";
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
+let strng = document.getElementsByTagName("strong");
+	for(let i = 0;i< strng.length;i++) {
+		strng[i].style.color = "black";
+	}
 
     
 }
